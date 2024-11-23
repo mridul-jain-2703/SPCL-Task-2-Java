a639de1d8fbde2d28d61630483f67690e0bbfdc9
mridul-jain-2703 <73199019+mridul-jain-2703@users.noreply.github.com>
2024-11-24 05:26:11 +0530

    Add files via upload

---
+++ storage-service/src/main/java/com/filesystem/storage_service/entity/FileData.java
@@ -0,0 +1,29 @@
+package com.filesystem.storage_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+
+@Entity
+@Table(name = "FILE_DATA")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FileData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String type;
+    private String filePath;
+}
---
+++ storage-service/src/main/java/com/filesystem/storage_service/repository/FileDataRepository.java
@@ -0,0 +1,10 @@
+package com.filesystem.storage_service.repository;
+
+import com.filesystem.storage_service.entity.FileData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FileDataRepository extends JpaRepository<FileData,Integer> {
+    Optional<FileData> findByName(String fileName);
+}
